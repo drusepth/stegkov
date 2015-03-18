976f7b9926448691832b9a4b68148fc7e263e36d
Andrew Brown <drusepth@gmail.com>
2015-03-17 22:48:00 -0500

    abstract stuff to shared object for encoding

--- encode.rb
+++ encode.rb
@@ -1,33 +1,11 @@
 #!/usr/bin/ruby
-unencoded = ARGV[0] || "You should have passed in a string"
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each{|file| require file}
 
-puts "Building dictionary"
-dict = File.foreach('/usr/share/dict/american-english').map(&:downcase).map(&:strip).map{|word| word.tr('^A-Za-z', '')}.uniq.select{|word| word.length > 3}
-
-puts "Generating key"
-possible_keys = ARGV[1] || begin
-  ('a'..'z').to_a - unencoded.downcase.split(' ').map{|i| i[0]}
-end
-key = dict.select{|word| possible_keys.include? word[0]}.sample
-key_index = -1
-
-def random_words dict, num, key
-  (1..num).map do |context_word|
-    dict.reject{|word| word[0] == key}.sample
-  end
-end
+unencoded_message = ARGV[0] || "You should have passed in a string"
 
 puts "Stegoing message"
-corpus = unencoded.downcase.split(' ').map do |unencoded_word|
-  key_index = key_index == key.length ? 0 : key_index + 1
-  [
-    random_words(dict, rand(15), key[key_index]),          # Random number of prefixing words
-    dict.select{|word| word[0] == key[key_index]}.sample,  # Sentinel word
-    unencoded_word,                                        # Next word to inject
-    random_words(dict, rand(5), key[key_index])            # Random number of affixing words
-  ]
-end.flatten.join(' ').capitalize
+encoded_message, key = Stegkov.new.encode unencoded_message
 
 # Output results
 puts "Done."
-puts "Your key is #{key}", "Message: #{corpus}"
+puts "Your key is #{key}", "Message: #{encoded_message}"
---
+++ lib/lib.rb
@@ -0,0 +1,35 @@
+class Stegkov
+  def dictionary
+    # todo cache this
+    File.foreach('/usr/share/dict/american-english').map(&:downcase).map(&:strip).map{|word| word.tr('^A-Za-z', '')}.uniq.select{|word| word.length > 3}
+  end
+
+  def generate_key_for unencoded_message
+    dictionary.select{|word| (('a'..'z').to_a - unencoded_message.downcase.split(' ').map{|i| i[0]}).include? word[0]}.sample
+  end
+
+  def encode unencoded_message
+    key = generate_key_for unencoded_message
+    key_index = -1
+
+    [unencoded_message.downcase.split(' ').map do |unencoded_word|
+      key_index = key_index == key.length ? 0 : key_index + 1
+      [
+        random_words(rand(15), key[key_index]),                      # Random number of prefixing words
+        dictionary.select{|word| word[0] == key[key_index]}.sample,  # Sentinel word
+        unencoded_word,                                              # Next word to inject
+        random_words(rand(5), key[key_index])                        # Random number of affixing words
+      ]
+    end.flatten.join(' ').capitalize, key]
+  end
+
+  def decode encoded_message, key
+
+  end
+
+  def random_words num, key
+    (1..num).map do |context_word|
+      dictionary.reject{|word| word[0] == key}.sample
+    end
+  end
+end
